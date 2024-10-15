80e4bd571e44035fa8fe232d643641b12e24e7c4
Guilherme Macedo <macedo.lac157@gmail.com>
2024-10-14 22:22:27 -0300

    projeto criado

---
+++ src/test/java/com/aurorabeatriz/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurorabeatriz.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
