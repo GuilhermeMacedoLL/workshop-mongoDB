3d0a49844d811b94bd9e638632ad8fc9678b08e5
Guilherme Macedo <macedo.lac157@gmail.com>
2024-10-16 00:24:22 -0300

    Obtendo um post por id

---
+++ src/main/java/com/aurorabeatriz/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.aurorabeatriz.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aurorabeatriz.workshopmongo.domain.Post;
+import com.aurorabeatriz.workshopmongo.repository.PostRepository;
+import com.aurorabeatriz.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+ }
